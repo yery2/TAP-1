a499922e6581dbe1d43147dc52905add46b53462
yery2 <Yeray.victor@gmail.com>
2026-09-21 23:12:50 +0200

    Ex Lab 1 intento

--- TAP1/src/bank/Bank.java
+++ TAP1/src/bank/Bank.java
@@ -1,7 +1,15 @@
 package bank;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.security.PublicKey;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by pedro on 9/14/15.
@@ -10,6 +18,7 @@ public class Bank {
     private String name;
     private List<Account> accounts;
     private List<Customer> customers;
+    private static double premi = 300.00;
 
     public Bank(String name) {
         this.name = name;
@@ -46,4 +55,51 @@ public class Bank {
         for (Account acc:accounts)
             acc.revision();
     }
+    
+    public void lottery(){
+        Random rand = new Random();
+        List <Account> accountsList = getAccounts();
+        int guanyador = rand.nextInt(accounts.size());
+        accountsList.get(guanyador).deposit(premi);
+
+    }
+
+    public void storage(String ruta){
+        try {
+            FileOutputStream f = new FileOutputStream(ruta);
+            ObjectOutputStream fos = new ObjectOutputStream(f);
+            fos.writeObject(this);
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch(IOException e2){
+            e2.printStackTrace();
+        }
+    }
+
+    public void recovery(String ruta){
+        Bank b = Bank.from_file(ruta);
+        this.name = b.name;
+        this.accounts = b.accounts;
+        this.customers = b.customers;
+    }
+
+    public static Bank from_file(String ruta){
+        Bank b = null;
+
+        try {
+            ObjectInputStream fis = new ObjectInputStream(new FileInputStream(ruta));
+            b = (Bank) fis.readObject();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e2) {
+            e2.printStackTrace();
+        } catch (ClassNotFoundException e3) {
+            e3.printStackTrace();
+        }
+
+        return b;
+    }
+
+
 }
--- TAP1/src/bank/Customer.java
+++ TAP1/src/bank/Customer.java
@@ -11,15 +11,22 @@ public class Customer {
         this.id = id;
         this.name = name;
     }
-
+    /**
+     * Metode per retornar l' Id
+     * @return id
+     */
     public String getId(){
         return id;
     }
-
+    /**
+     * Metode per retornar el nom
+     * @return name
+     */
     public String getName(){
         return name;
     }
 
+    
     @Override
     public String toString() {
         return "Customer{" +
